9d824b9c02dd00bc2a9e482b5a3c87df83595aec
Maksim Lesnikov <maksimlesnikov@Maksims-MacBook-Air.local>
2026-02-26 12:54:59 +0300

    Update Apps Script URL

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 export const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzlbDxRhNBf_Pt03kWBZiARVMv5cVKH3Ab8BdWZLk3EjWMhkQle5-LfkqMnmTjGTTnQYg/exec";
+  "https://script.google.com/macros/s/AKfycbyXRdNs8CMviF5xZJ6ec-0o8VqtbhBtLb6X3c9L9F_-oQdMN7uQHClVO5bzzn8m3tF8Sw/exec";
 export const OWNER_CURRENCY = "RUB";
 export const USE_LOCAL_SAMPLE_DATA_WHEN_NO_API = true;
